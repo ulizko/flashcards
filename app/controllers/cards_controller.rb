b8995aef018edea554ce365418ce5446b86d1c50
shurikovich <shurikovich@bigmir.net>
2016-05-09 08:07:38 +0000

    add after_filter for show

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,12 @@
 class CardsController < ApplicationController
-  
+
   before_filter :find_card, only: [:show, :edit, :update, :destroy]
-  
+  after_filter  :set_review_date, only: :show
+
   def new
     @card = Card.new
   end
-  
+
   def create
     @card = Card.new(card_params)
     if @card.save
@@ -14,17 +15,17 @@ class CardsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def index
     @cards = Card.all
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
     if @card.update(card_params)
       redirect_to action: 'index'
@@ -32,18 +33,22 @@ class CardsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @card.destroy
     redirect_to cards_path
   end
-  
+
   private
-  
+
+  def set_review_date
+    @card.update_attribute(:review_date, Time.now + 3 * 60 * 60 * 24)
+  end
+
   def card_params
     params.require(:card).permit(:original_text, :translated_text)
   end
-  
+
   def find_card
     @card = Card.find(params[:id])
   end
