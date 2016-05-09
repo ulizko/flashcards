9130a20855537ffe34cf90c3a33dfa0f88f61013
shurikovich <shurikovich@bigmir.net>
2016-05-09 07:52:16 +0000

    add validates

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,50 @@
 class CardsController < ApplicationController
   
+  before_filter :find_card, only: [:show, :edit, :update, :destroy]
+  
+  def new
+    @card = Card.new
+  end
+  
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to action: 'index'
+    else
+      render 'new'
+    end
+  end
+  
   def index
     @cards = Card.all
   end
   
   def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @card.update(card_params)
+      redirect_to action: 'index'
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @card.destroy
+    redirect_to cards_path
+  end
+  
+  private
+  
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
+  end
+  
+  def find_card
+    @card = Card.find(params[:id])
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,24 @@
 class Card < ActiveRecord::Base
+  
+  validates_presence_of :original_text, :translated_text
+  validates_uniqueness_of :original_text, :translated_text
+  validate :original_text_eql_translated_text
+  validates :original_text, format: { with: /\A[A-Za-z][a-z]+\Z/,
+    message: ' should contain only the English alphabet and can\'t contain spaces in start of string' }
+  validates :translated_text, format: { with: /\A[А-Яа-я][а-я]+\Z/, 
+    message: ' should contain only the Cyrillic alphabet and can\'t contain spaces in start of string' }
+  validates :review_date, presence: true
+  
+  before_validation :set_review_date
+  
+  private
+  
+  def set_review_date
+    self.update_attribute(:review_date, Time.now + 3 * 60 * 60 * 24)
+  end
+  
+  def original_text_eql_translated_text
+    errors.add(:original_text, 'need not be equal to translated text') if 
+      original_text.downcase == translated_text.downcase
+  end
 end
