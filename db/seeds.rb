795cefcb077ebd4e241bcbba8d7d16c583503cc5
shurikovich <shurikovich@bigmir.net>
2016-05-10 17:06:02 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+
+page = Nokogiri::HTML(open('http://4flaga.ru/d_1_1000.html'))
+original = page.css('p.newsgr a').children.map(&:text).map(&:strip)
+
+words = page.css('p.newsgr').map(&:text)
+  .first.gsub(/[а-яё]+[а-яёa-z\s,;().-]+/).map(&:strip)
+    .delete_if { |d| d == '' }.map { |v| v.gsub(/[a-z]/, '') }
+
+ziped = original.zip(words)
+vocabulary = ziped.map { |v| [:original_text, :translated_text].zip(v).to_h }
+
+vocabulary.each { |v| Card.create(v) }
