089d671ed72954ea7c5210d901e07e61089bae4d
shurikovich <shurikovich@bigmir.net>
2016-05-18 19:11:43 +0000

    Fix: add condition to callback

--- spec/factories/card.rb
+++ spec/factories/card.rb
@@ -2,7 +2,5 @@ FactoryGirl.define do
   factory :card, class: Card do
     original_text 'car'
     translated_text 'машина'
-    
-    after(:build) { |card| card.class.skip_callback(:create, :before, :set_review_date) }
   end
 end
