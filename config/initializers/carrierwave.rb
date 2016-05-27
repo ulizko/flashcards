12e608d161b74bbb9057060469258e33cd1d275b
shurikovich <shurikovich@bigmir.net>
2016-05-27 12:22:01 +0000

    Try fix travis

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:              'AWS',
-    aws_access_key_id:      ENV['S3_KEY'],
-    aws_secret_access_key:  ENV['S3_SECRET'],
-    region:                 ENV['S3_REGION']
-  }
-  config.fog_directory = ENV['S3_BUCKET_NAME']
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-  if Rails.env.test? || Rails.env.cucumber?
+  if Rails.env.test?
     config.storage = :file
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp"
   else
     config.storage = :fog
+    config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:      ENV['S3_KEY'],
+    aws_secret_access_key:  ENV['S3_SECRET'],
+    region:                 ENV['S3_REGION']
+    }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
   end
 end
