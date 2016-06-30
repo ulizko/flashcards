5f7e6f70e547802e03b46273bc84b85f56e863af
shurikovich <shurikovich@bigmir.net>
2016-06-30 18:10:54 +0000

    Fix travis

--- spec/mailers/previews/notifications_mailer_preview.rb
+++ spec/mailers/previews/notifications_mailer_preview.rb
@@ -1,6 +1,5 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notifications_mailer
 class NotificationsMailerPreview < ActionMailer::Preview
-
   def pending_cards
     user = User.last
     NotificationsMailer.pending_cards(user)
