8d02cddabc96d321ce2ff5ce04dbaf550fdbc9f0
shurikovich <shurikovich@bigmir.net>
2016-07-07 09:36:27 +0000

    Localize test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     address:        ENV['MAILGUN_SMTP_SERVER'],
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:         'http://thawing-wave-73151.herokuapp.com',
+    domain:         ENV['DOMAIN'],
     authentication: :plain
   }
 end
--- spec/features/card_check_spec.rb
+++ spec/features/card_check_spec.rb
@@ -5,27 +5,29 @@ RSpec.describe "Card check", type: :feature do
   let!(:card) { create(:card, user: user) }
   before(:each) do
     visit root_path
-    click_link "Login"
+    click_link I18n.t('layouts.application.login')
     fill_in :email, with: user[:email]
     fill_in :password, with: 'qwerty123'
-    click_button "Login"
+    click_button I18n.t('layouts.application.login')
   end
   it 'should be Right' do
     expect(page).to have_content('машина')
     fill_in :check_check_translate, with: 'Car'
-    click_button 'Check card'
-    expect(page).to have_content('Right. Let\'s check next card.')
+    click_button I18n.t('home.form.check_card')
+    expect(page).to have_content(I18n.t('cards.check.right'))
   end
 
   it 'should be Oops' do
     fill_in :check_check_translate, with: 'acr'
-    click_button 'Check card'
-    expect(page).to have_content('Oops! Your answer is correct, but')
+    click_button I18n.t('home.form.check_card')
+    expect(page).to have_content(I18n.t('cards.check.oops',
+                                        original_text: card.original_text,
+                                        check_translate: 'acr'))
   end
 
   it 'should be Wrong' do
     fill_in :check_check_translate, with: 'bike'
-    click_button 'Check card'
-    expect(page).to have_content('Wrong! Try another card.')
+    click_button I18n.t('home.form.check_card')
+    expect(page).to have_content(I18n.t('cards.check.wrong'))
   end
 end
